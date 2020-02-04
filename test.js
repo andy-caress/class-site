65100c00735c314f7b05845f1049928697436b5f
andy-caress <54376326+andy-caress@users.noreply.github.com>
2020-02-04 00:06:54 -0500

    General design
    Updated design

---
+++ test.js
@@ -0,0 +1,10 @@
+//javascript is used to implement behavior
+
+let name = 'mosh';
+console.log('name');
+//let = variable, inputing variable for cpu to remember
+
+const interestRate = .3;
+interestRate = 1;
+console.log('interestRate');
+//const = constant, use constant when you don't need to reassign a variable
